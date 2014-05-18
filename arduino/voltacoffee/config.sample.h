ce1b1c8bf5f146029ea4ea767811c7449a473ba8
Hatem Nassrat <hnassrat@gmail.com>
2014-05-18 12:38:45 -0300

    Added note.

--- arduino/voltacoffee/config.sample.h
+++ arduino/voltacoffee/config.sample.h
@@ -1,3 +1,5 @@
+// Rename this file, config.h in this current directory
+
 #define WLAN_SSID       "WlanSSID"        // cannot be longer than 32 characters!
 #define WLAN_PASS       "WlanPASS"
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
